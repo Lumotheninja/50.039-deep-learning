7eaa4f70f7b17ec139000179ed07df56af9a436d
lumotheninja <wentat126@yahoo.com>
2019-05-10 16:14:38 +0800

    codinghw 5

--- codinghw5/hw5.py
+++ codinghw5/hw5.py
@@ -46,14 +46,18 @@ class RNN(nn.Module):
         self.layer_size = layer_size
         self.class_size = class_size
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=layer_size, batch_first=True)
-        self.fc = torch.nn.Linear(layer_size*hidden_size, class_size)
+        self.fc = torch.nn.Linear(hidden_size, class_size)
+        #self.fc = torch.nn.Linear(layer_size*hidden_size, class_size)
 
     def forward(self, input):
         num_batch, _, __ = input.shape
+        input = torch.nn.utils.rnn.pack_padded_sequence(input, [v.size(0) for v in input], batch_first=True)
         h0 = torch.zeros(self.layer_size, num_batch, self.hidden_size)
         c0 = torch.zeros(self.layer_size, num_batch, self.hidden_size)
         output, (hidden, cell)= self.lstm(input, (h0, c0))
-        return (self.fc(hidden.view(num_batch, self.layer_size * self.hidden_size)))
+        output, _ = torch.nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
+        return self.fc(output[:, -1, :])
+        # return (self.fc(hidden.view(num_batch, self.layer_size * self.hidden_size)))
 
 def unicodeToAscii(s):
         return ''.join(
@@ -84,8 +88,6 @@ def train(model, loss_fn, device, train_loader, optimizer):
         optimizer.step()
         samples_trained += data.size()[0]
         print('Trained %d samples, loss: %10f' %(samples_trained, training_loss/samples_trained), end="\r")
-        del data
-        del target
     training_loss /= samples_trained
     return training_loss
 
@@ -104,8 +106,6 @@ def test(model, loss_fn, device, test_loader):
             samples_tested += data.size()[0]
             #print (pred)
             print('Tested %d samples, loss: %10f' %(samples_tested, test_loss/samples_tested), end="\r")
-            del data
-            del target
     accuracy = correct_pred/samples_tested
     test_loss /= samples_tested
     return accuracy, test_loss
@@ -158,7 +158,6 @@ def train_test_test(model, loss_fn, device, optimizer, epoch, name):
     '''
 
 def collate_fn(batch):
-    
     data = torch.nn.utils.rnn.pad_sequence([item[0] for item in batch], batch_first=True)
     target = torch.tensor([item[1] for item in batch])
     #print (data.size())
@@ -166,7 +165,7 @@ def collate_fn(batch):
 
 test_split = .2
 random_seed= 24
-batch_size =  2
+batch_size =  30
 category_list = []
 train_list = []
 test_list = []
@@ -196,13 +195,13 @@ test_dataset = LanguageDataset(test_list, test_label_list)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 
-n_hidden = 100
+n_hidden = 50
 n_layer= 2
 
 rnn = RNN(train_dataset.getInputSize(), n_hidden, n_layer, n_classes)
 epoch = 10
 loss = nn.CrossEntropyLoss()
 learning_rate = 0.01
-optimizer=torch.optim.SGD(rnn.parameters(),lr=learning_rate, momentum = 0.01)
+optimizer=torch.optim.Adam(rnn.parameters(),lr=learning_rate)
 device = torch.device('cpu')
-train_test_test(rnn, loss, device, optimizer, epoch, 'model_4')
+train_test_test(rnn, loss, device, optimizer, epoch, 'batchsize30_h50')
